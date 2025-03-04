b5a8aa2ceae6dd7d3a2e48fc6409add67b166a9f
chintan <chintan@gmail.com>
2025-03-04 08:12:18 +0530

    all

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const app = express()
 const PORT = 3000
 const cors = require("cors")
 const Razorpay = require('razorpay')
+var nodemailer = require('nodemailer');
 
 app.use(cors())
-
+app.use(express.json())
 app.get("/pay",(req,resp)=>{
 
     const amt =Number(req.query.amt)
@@ -23,6 +24,40 @@ app.get("/pay",(req,resp)=>{
 
 
 })
+
+app.post("/mail",(req,resp)=>{
+
+  
+  
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'chintan.tops@gmail.com',
+      pass: 'zotp msxt cvrk sgyz'
+    }
+  });
+
+
+  var mailOptions = {
+    from: 'chintan.tops@gmail.com',
+    to: req.body.to,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+     resp.send(error);
+    } else {
+      resp.send('Email sent: ' + info.response);
+    }
+  });
+
+})
+
+
+
 
 app.listen(PORT,()=>{
     console.log("server running on port : "+PORT);
